57a9679267dbd0692493620746b7363757af4a79
Jose Caballero <jcaballero@bnl.gov>
2017-03-22 13:57:15 -0400

    1490205435

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # APPA
 an Alternative framework for Particle Physics Analysis
 
+===============================================================================
+    DEPLOYMENT
+===============================================================================
+
+As root:
+
+    $ git clone https://github.com/jose-caballero/APPA.git
+    $ cd APPA
+    $ python setup.py bdist_rpm
+    $ rpm -Uhv dist/appa-<version>.noarch.rpm
+
+As user:
+
+    $ git clone https://github.com/jose-caballero/APPA.git
+    $ cd APPA
+    $ python setup.py install --home=$HOME
+    $ export PYTHONPATH=$HOME/lib/python:$PYTHONPATH
+
--- setup.py
+++ setup.py
@@ -9,62 +9,18 @@ import os
 import re
 import sys
 
-from appa import infoservice
-release_version=infoservice.__version__
+from appa import aframework
+release_version=aframework.__version__
 
 from distutils.core import setup
 from distutils.command.install import install as install_org
 from distutils.command.install_data import install_data as install_data_org
 
-
-# ===========================================================
-#                D A T A     F I L E S 
-# ===========================================================
-
-libexec_files = ['libexec/%s' %file for file in os.listdir('libexec') if os.path.isfile('libexec/%s' %file)]
-systemd_files = [ 'etc/appa', ]
-etc_files = ['etc/appa.conf', ]
-
-# -----------------------------------------------------------
-
-rpm_data_files=[('/usr/libexec', libexec_files),
-                ('/etc/appa', etc_files),
-                #('/usr/share/doc/appa', docs_files),                                        
-               ]
-
-
-home_data_files=[('etc', libexec_files),
-                 ('etc', etc_files),
-                ]
-
-# -----------------------------------------------------------
-
-def choose_data_files():
-    rpminstall = True
-    userinstall = False
-     
-    if 'bdist_rpm' in sys.argv:
-        rpminstall = True
-
-    elif 'install' in sys.argv:
-        for a in sys.argv:
-            if a.lower().startswith('--home'):
-                rpminstall = False
-                userinstall = True
-                
-    if rpminstall:
-        return rpm_data_files
-    elif userinstall:
-        return home_data_files
-    else:
-        # Something probably went wrong, so punt
-        return rpm_data_files
-       
 # ===========================================================
 
 # setup for distutils
 setup(
-    name="APPA",
+    name="appa",
     version=release_version,
     description='APPA package',
     long_description='''This package contains the package APPA''',
@@ -86,5 +42,5 @@ setup(
                'bin/appa',
               ],
     
-    data_files = choose_data_files()
+    data_files = []
 )
